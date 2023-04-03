838b9550deaa6a25c23d9bea35fdcec87f0dc6db
MDENDAN <md.mollick15697@sakec.ac.in>
2023-04-04 00:20:14 +0530

    SplashScreen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:demo_arduino/bluetooth.dart';
+import 'package:flutter/material.dart';
+
+class splashscreen extends StatefulWidget {
+  const splashscreen({super.key});
+
+  @override
+  State<splashscreen> createState() => _splashscreenState();
+}
+
+class _splashscreenState extends State<splashscreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+        Duration(seconds: 5),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => BluetoothApp())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child: FlutterLogo(size: MediaQuery.of(context).size.height * 0.6));
+  }
+}
